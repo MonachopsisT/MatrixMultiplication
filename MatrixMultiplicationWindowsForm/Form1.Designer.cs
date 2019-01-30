1ae0d9284f1efa3a82e9f185e0a46622a62b3918
MonachopsisT <teditod07@gmail.com>
2019-01-30 21:01:00 +0000

    Initial commit

---
+++ MatrixMultiplicationWindowsForm/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.matrix1 = new System.Windows.Forms.DataGridView();
+            this.matrix2 = new System.Windows.Forms.DataGridView();
+            this.enter1 = new System.Windows.Forms.Button();
+            this.enter2 = new System.Windows.Forms.Button();
+            this.row1 = new System.Windows.Forms.TextBox();
+            this.column1 = new System.Windows.Forms.TextBox();
+            this.row2 = new System.Windows.Forms.TextBox();
+            this.column2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.matrix1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.matrix2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.result)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // matrix1
+            // 
+            this.matrix1.AllowUserToAddRows = false;
+            this.matrix1.AllowUserToDeleteRows = false;
+            this.matrix1.AllowUserToResizeColumns = false;
+            this.matrix1.AllowUserToResizeRows = false;
+            this.matrix1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matrix1.ColumnHeadersVisible = false;
+            this.matrix1.Location = new System.Drawing.Point(12, 74);
+            this.matrix1.Name = "matrix1";
+            this.matrix1.RowHeadersVisible = false;
+            this.matrix1.Size = new System.Drawing.Size(246, 156);
+            this.matrix1.TabIndex = 0;
+            // 
+            // matrix2
+            // 
+            this.matrix2.AllowUserToAddRows = false;
+            this.matrix2.AllowUserToDeleteRows = false;
+            this.matrix2.AllowUserToResizeColumns = false;
+            this.matrix2.AllowUserToResizeRows = false;
+            this.matrix2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matrix2.ColumnHeadersVisible = false;
+            this.matrix2.Location = new System.Drawing.Point(346, 74);
+            this.matrix2.Name = "matrix2";
+            this.matrix2.RowHeadersVisible = false;
+            this.matrix2.Size = new System.Drawing.Size(246, 156);
+            this.matrix2.TabIndex = 1;
+            // 
+            // enter1
+            // 
+            this.enter1.Location = new System.Drawing.Point(150, 23);
+            this.enter1.Name = "enter1";
+            this.enter1.Size = new System.Drawing.Size(75, 23);
+            this.enter1.TabIndex = 2;
+            this.enter1.Text = "Enter";
+            this.enter1.UseVisualStyleBackColor = true;
+            this.enter1.Click += new System.EventHandler(this.enter1_Click);
+            // 
+            // enter2
+            // 
+            this.enter2.Location = new System.Drawing.Point(458, 29);
+            this.enter2.Name = "enter2";
+            this.enter2.Size = new System.Drawing.Size(75, 23);
+            this.enter2.TabIndex = 3;
+            this.enter2.Text = "Enter";
+            this.enter2.UseVisualStyleBackColor = true;
+            this.enter2.Click += new System.EventHandler(this.enter2_Click);
+            // 
+            // row1
+            // 
+            this.row1.Location = new System.Drawing.Point(82, 12);
+            this.row1.Name = "row1";
+            this.row1.Size = new System.Drawing.Size(62, 20);
+            this.row1.TabIndex = 4;
+            // 
+            // column1
+            // 
+            this.column1.Location = new System.Drawing.Point(82, 38);
+            this.column1.Name = "column1";
+            this.column1.Size = new System.Drawing.Size(62, 20);
+            this.column1.TabIndex = 5;
+            // 
+            // row2
+            // 
+            this.row2.Location = new System.Drawing.Point(390, 19);
+            this.row2.Name = "row2";
+            this.row2.Size = new System.Drawing.Size(62, 20);
+            this.row2.TabIndex = 6;
+            // 
+            // column2
+            // 
+            this.column2.Location = new System.Drawing.Point(390, 44);
+            this.column2.Name = "column2";
+            this.column2.Size = new System.Drawing.Size(62, 20);
+            this.column2.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Row:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Column:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(355, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Row:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(343, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Column: ";
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(1, 236);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(75, 23);
+            this.Calculate.TabIndex = 12;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // result
+            // 
+            this.result.AllowUserToAddRows = false;
+            this.result.AllowUserToDeleteRows = false;
+            this.result.AllowUserToResizeColumns = false;
+            this.result.AllowUserToResizeRows = false;
+            this.result.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.result.ColumnHeadersVisible = false;
+            this.result.Location = new System.Drawing.Point(679, 74);
+            this.result.Name = "result";
+            this.result.RowHeadersVisible = false;
+            this.result.Size = new System.Drawing.Size(246, 156);
+            this.result.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(281, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 55);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "*";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(609, 126);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 55);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 522);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.column2);
+            this.Controls.Add(this.row2);
+            this.Controls.Add(this.column1);
+            this.Controls.Add(this.row1);
+            this.Controls.Add(this.enter2);
+            this.Controls.Add(this.enter1);
+            this.Controls.Add(this.matrix2);
+            this.Controls.Add(this.matrix1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.matrix1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.matrix2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.result)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView matrix1;
+        private System.Windows.Forms.DataGridView matrix2;
+        private System.Windows.Forms.Button enter1;
+        private System.Windows.Forms.Button enter2;
+        private System.Windows.Forms.TextBox row1;
+        private System.Windows.Forms.TextBox column1;
+        private System.Windows.Forms.TextBox row2;
+        private System.Windows.Forms.TextBox column2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.DataGridView result;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ MatrixMultiplicationWindowsForm/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static int kolona1;
+        static int red1;
+        static int kolona2;
+        static int red2;
+        static double[,] CalcMatrix(double[,] a, double[,] b)
+        {
+            double[,] MatrixResult = new double[a.GetLength(0), b.GetLength(1)];
+            if (a.GetLength(1) == b.GetLength(0))
+            {
+                for (int i = 0; i < a.GetLength(0); i++)
+                    for (int j = 0; j < b.GetLength(1); j++)
+                    {
+                        MatrixResult[i, j] = 0;
+                        for (int k = 0; k < b.GetLength(0); k++)
+                        {
+                            MatrixResult[i, j] += a[i, k] * b[k, j];
+                        }
+                    }
+            }
+            else
+            {
+                MatrixResult = null;
+                MessageBox.Show("Error");
+            }
+            return MatrixResult;
+        }
+
+
+        private void enter1_Click(object sender, EventArgs e)
+        {
+            red1 = int.Parse(row1.Text);
+            kolona1 = int.Parse(column1.Text);
+
+            for (int i = 0; i < kolona1; i++)
+            {
+                matrix1.Columns.Add("", "");
+                DataGridViewColumn column = matrix1.Columns[i];
+                column.Width = 50;
+            }
+            matrix1.Rows.Add(red1);
+            enter1.Enabled = false;
+            column1.Enabled = false;
+            row1.Enabled = false;
+        }
+
+        private void enter2_Click(object sender, EventArgs e)
+        {
+            red2 = int.Parse(row2.Text);
+            kolona2 = int.Parse(column2.Text);
+            for (int i = 0; i < kolona2; i++)
+            {
+                matrix2.Columns.Add("", "");
+                DataGridViewColumn column = matrix2.Columns[i];
+                column.Width = 50;
+            }
+            matrix2.Rows.Add(red2);
+            enter2.Enabled = false;
+            column2.Enabled = false;
+            row2.Enabled = false;
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+
+            if (enter1.Enabled == false && enter2.Enabled == false)
+                if (kolona1 != red2)
+                {
+                    MessageBox.Show("Error");
+                }
+
+                else
+                {
+                    
+                    for (int i = 0; i < kolona2; i++)
+                    {
+                        result.Columns.Add("", "");
+                        DataGridViewColumn column = result.Columns[i];
+                        column.Width = 50;
+                    }
+                    result.Rows.Add(red1);
+
+                    double[,] matrix_1; 
+                    matrix_1 = new double[red1, kolona1];
+                    double[,] matrix_2;
+                    matrix_2 = new double[red2, kolona2];
+
+                    for (int i = 0; i < red1; i++)
+                    {
+                        for (int j = 0; j < kolona1; j++)
+                        {
+                            matrix_1[i, j] = double.Parse(matrix1[j, i].Value.ToString());
+                        }
+                    }
+                    for (int i = 0; i < red2; i++)
+                    {
+                        for (int j = 0; j < kolona2; j++)
+                        {
+                            matrix_2[i, j] = double.Parse(matrix2[j, i].Value.ToString());
+                        }
+                    }
+                    double[,] result_matrix = new double[red1, kolona2];
+
+                    result_matrix = CalcMatrix(matrix_1,matrix_2);
+                    for (int i = 0; i < red1; i++)
+                    {
+                        for (int k = 0; k < kolona2; k++)
+                        {
+                            result[k, i].Value = result_matrix[i, k];
+                        }
+                    }
+                }
+
+        }
+
+
+
+    }
+}
